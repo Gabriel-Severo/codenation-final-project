1dde8f27d0b8c0178b37387d7d45c239d4cfdb20
gabriel-severo <gabriel.ivi@hotmail.com>
2020-05-13 12:04:40 -0300

    Add products component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import TopBar from './components/TopBar'
+import Products from './components/Products'
 
 class App extends React.Component {
   render(){
     return (
       <React.Fragment>
         <TopBar/>
+        <Products/>
       </React.Fragment>
     )
   }
---
+++ src/components/Products.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import './Products.css'
+
+class Products extends React.Component{
+    render(){
+        return(
+            <section className="products">
+                <div className="container">
+                    <div className="number__items">22 items</div>
+                    <div className="products__grid">
+                        <div className="product__box">
+                            <div className="product__image">
+                                <img src="https://d3l7rqep7l31az.cloudfront.net/images/products/20002605_615_catalog_1.jpg?1460136912" alt="VESTIDO TRANSPASSE BOW"/>
+                            </div>
+                            <div className="product__info">
+                                <h3 className="product__name">VESTIDO TRANSPASSE BOW</h3>
+                                <span className="product__price">R$ 199,90</span>
+                            </div>
+                        </div>
+                        <div className="product__box">
+                            <div className="product__image">
+                                <img src="https://d3l7rqep7l31az.cloudfront.net/images/products/20002605_615_catalog_1.jpg?1460136912" alt="VESTIDO TRANSPASSE BOW"/>
+                            </div>
+                            <div className="product__info">
+                                <h3 className="product__name">VESTIDO TRANSPASSE BOW</h3>
+                                <span className="product__price">R$ 199,90</span>
+                            </div>
+                        </div>
+                        <div className="product__box">
+                            <div className="product__image">
+                                <img src="https://d3l7rqep7l31az.cloudfront.net/images/products/20002605_615_catalog_1.jpg?1460136912" alt="VESTIDO TRANSPASSE BOW"/>
+                            </div>
+                            <div className="product__info">
+                                <h3 className="product__name">VESTIDO TRANSPASSE BOW</h3>
+                                <span className="product__price">R$ 199,90</span>
+                            </div>
+                        </div>
+                        <div className="product__box">
+                            <div className="product__image">
+                                <img src="https://d3l7rqep7l31az.cloudfront.net/images/products/20002605_615_catalog_1.jpg?1460136912" alt="VESTIDO TRANSPASSE BOW"/>
+                            </div>
+                            <div className="product__info">
+                                <h3 className="product__name">VESTIDO TRANSPASSE BOW</h3>
+                                <span className="product__price">R$ 199,90</span>
+                            </div>
+                        </div>
+                        <div className="product__box">
+                            <div className="product__image">
+                                <img src="https://d3l7rqep7l31az.cloudfront.net/images/products/20002605_615_catalog_1.jpg?1460136912" alt="VESTIDO TRANSPASSE BOW"/>
+                            </div>
+                            <div className="product__info">
+                                <h3 className="product__name">VESTIDO TRANSPASSE BOW</h3>
+                                <span className="product__price">R$ 199,90</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default Products
