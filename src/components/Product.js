2254cfd52b7b821c050d356d4cec0a63a5ae7b77
gabriel-severo <gabriel.ivi@hotmail.com>
2020-05-14 11:43:40 -0300

    Add discount in product info

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,7 +10,14 @@ class Product extends React.Component{
                 </div>
                 <div className="product__info">
                     <h3 className="product__name">{this.props.product.name}</h3>
-                    <span className="product__price">{this.props.product.regular_price}</span>
+                    <div className="product__prices">
+                        {this.props.product.discount_percentage ?
+                        <>
+                            <span className="product__price strikethrough">{this.props.product.regular_price}</span>
+                            <span class="product__price--new">{this.props.product.actual_price}</span>
+                        </> : <span className="product__price">{this.props.product.regular_price}</span>
+                        }
+                    </div>
                 </div>
             </div>
         )
