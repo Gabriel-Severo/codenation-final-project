96ba1c5d6147d3553b752fbf9af3af61c495f3ea
gabriel-severo <gabriel.ivi@hotmail.com>
2020-05-14 12:00:44 -0300

    Add class in product image

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,9 +4,9 @@ class Product extends React.Component{
     render(){
         return(
             <div className="product__box">
-                <div className="product__image">
+                <div className="image__box">
                     {this.props.product.discount_percentage && <span class="discount">{this.props.product.discount_percentage}</span>}
-                    <img src={this.props.product.image ? this.props.product.image : 'https://via.placeholder.com/470x594/FFFFFF/?text=Imagem+Indispon%C3%ADvel'} alt={this.props.product.name}/>
+                    <img class="product__image" src={this.props.product.image ? this.props.product.image : 'https://via.placeholder.com/470x594/FFFFFF/?text=Imagem+Indispon%C3%ADvel'} alt={this.props.product.name}/>
                 </div>
                 <div className="product__info">
                     <h3 className="product__name">{this.props.product.name}</h3>
