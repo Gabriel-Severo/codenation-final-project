04ea128cb04c15569c8d3b542f630b2420102336
gabriel-severo <gabriel.ivi@hotmail.com>
2020-05-20 13:19:24 -0300

    Modified SearchResult component to function

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import './SearchResult.css'
-class SearchResult extends React.Component {
-    render() {
-        return (
-            <div className="product__result">
-                <div className="product__result__image">
-                    <img className="product__image" src={this.props.product.image ? this.props.product.image : 'https://via.placeholder.com/470x594/FFFFFF/?text=Imagem+Indispon%C3%ADvel'} alt="Produto"/>
-                </div>
-                <div className="product__result__name">{this.props.product.name}</div>
-                <div className="product__result__prices">
-                    <div className="product__result__price">{this.props.product.actual_price}</div>
-                    <div className="product__result__installments">{this.props.product.installments}</div>
-                </div>
+function SearchResult({product}) {
+    return (
+        <div className="product__result">
+            <div className="product__result__image">
+                <img className="product__image" src={product.image ? product.image : 'https://via.placeholder.com/470x594/FFFFFF/?text=Imagem+Indispon%C3%ADvel'} alt="Produto"/>
             </div>
-        )
-    }
+            <div className="product__result__name">{product.name}</div>
+            <div className="product__result__prices">
+                <div className="product__result__price">{product.actual_price}</div>
+                <div className="product__result__installments">{product.installments}</div>
+            </div>
+        </div>
+    )
 }
 
 export default SearchResult
