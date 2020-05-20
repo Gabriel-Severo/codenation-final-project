8ad2754ab680c4e6b9f830f4a1b06a5b87b68fcb
gabriel-severo <gabriel.ivi@hotmail.com>
2020-05-20 13:39:39 -0300

    Modified CartProduct component to function

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -1,35 +1,32 @@
 import React from 'react'
 import './CartProduct.css'
 
-
-class CartProduct extends React.Component {
-    render (){
-        return (
-            <div className="cart__product">
-                        <div className="cart__product__image">
-                            <img className="product__image" src="https://d3l7rqep7l31az.cloudfront.net/images/products/20002584_035_catalog_1.jpg?1459947139" alt="Produto"/>
-                        </div>
-                        <div className="cart__info">
-                            <div className="cart__product__name">CAMISA SUEDE SPAN</div>
-                            <div className="cart__product__size">Tam.: M</div>
-                            <div className="cart__product__quantity">
-                                <button className="cart__product__add">
-                                    <i className="fas fa-plus"></i>
-                                </button>
-                                <span className="cart__product__cont">1</span>
-                                <button className="cart__product__remove">
-                                    <i className="fas fa-minus"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <div className="cart__prices">
-                            <span className="cart__product__price">R$ 189,90</span>
-                            <span className="cart__product__installments">3x R$ 63,30</span>
-                        </div>
-                        <button className="cart__remove__item">Remover Item</button>
-                    </div>
-        )
-    }
+function CartProduct() {
+    return (
+        <div className="cart__product">
+            <div className="cart__product__image">
+                <img className="product__image" src="https://d3l7rqep7l31az.cloudfront.net/images/products/20002584_035_catalog_1.jpg?1459947139" alt="Produto"/>
+            </div>
+            <div className="cart__info">
+                <div className="cart__product__name">CAMISA SUEDE SPAN</div>
+                <div className="cart__product__size">Tam.: M</div>
+                <div className="cart__product__quantity">
+                    <button className="cart__product__add">
+                        <i className="fas fa-plus"></i>
+                    </button>
+                    <span className="cart__product__cont">1</span>
+                    <button className="cart__product__remove">
+                        <i className="fas fa-minus"></i>
+                    </button>
+                </div>
+            </div>
+            <div className="cart__prices">
+                <span className="cart__product__price">R$ 189,90</span>
+                <span className="cart__product__installments">3x R$ 63,30</span>
+            </div>
+            <button className="cart__remove__item">Remover Item</button>
+        </div>
+    )
 }
 
 export default CartProduct
