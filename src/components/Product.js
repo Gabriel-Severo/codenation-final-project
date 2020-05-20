9d2abc48efb31fd9518e4e4320cb7334f5cc7258
gabriel-severo <gabriel.ivi@hotmail.com>
2020-05-20 13:36:56 -0300

    Modified Product component to function

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,27 +1,25 @@
 import React from 'react'
 import './Product.css'
-class Product extends React.Component{
-    render(){
-        return(
-            <div className="product__box">
-                <div className="image__box">
-                    {this.props.product.discount_percentage && <span className="discount">{this.props.product.discount_percentage}</span>}
-                    <img className="product__image" src={this.props.product.image ? this.props.product.image : 'https://via.placeholder.com/470x594/FFFFFF/?text=Imagem+Indispon%C3%ADvel'} alt={this.props.product.name}/>
-                </div>
-                <div className="product__info">
-                    <h3 className="product__name">{this.props.product.name}</h3>
-                    <div className="product__prices">
-                        {this.props.product.discount_percentage ?
-                        <>
-                            <span className="product__price strikethrough">{this.props.product.regular_price}</span>
-                            <span className="product__price--new">{this.props.product.actual_price}</span>
-                        </> : <span className="product__price">{this.props.product.regular_price}</span>
-                        }
-                    </div>
+function Product({product}){
+    return(
+        <div className="product__box">
+            <div className="image__box">
+                {product.discount_percentage && <span className="discount">{product.discount_percentage}</span>}
+                <img className="product__image" src={product.image ? product.image : 'https://via.placeholder.com/470x594/FFFFFF/?text=Imagem+Indispon%C3%ADvel'} alt={product.name}/>
+            </div>
+            <div className="product__info">
+                <h3 className="product__name">{product.name}</h3>
+                <div className="product__prices">
+                    {product.discount_percentage ?
+                    <>
+                        <span className="product__price strikethrough">{product.regular_price}</span>
+                        <span className="product__price--new">{product.actual_price}</span>
+                    </> : <span className="product__price">{product.regular_price}</span>
+                    }
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Product
