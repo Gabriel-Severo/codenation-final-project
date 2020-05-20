7883bb3c84e282d9c357317c9371f16ce3545a26
gabriel-severo <gabriel.ivi@hotmail.com>
2020-05-20 13:41:00 -0300

    Modified Cart component to function

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,36 +1,34 @@
 import React from 'react'
 import './Cart.css'
 import CartProduct from './CartProduct'
-class Cart extends React.Component {
+function Cart() {
 
-    handleCloseCart(event){
+    function handleCloseCart(event){
         event.preventDefault()
         document.querySelector('.cart__modal').classList.add('modal__cart__hidden')
         document.querySelector('.app').classList.remove('blur')
     }
 
-    render(){
-        return(
-            <div className="cart__modal modal__cart__hidden">
-                <div className="cart__top">
-                    <button onClick={e => {this.handleCloseCart(e)}} className="return__button">
-                        <i className="fas fa-arrow-left"></i>
-                    </button>
-                    <strong>Sacola (0)</strong>
-                </div>
-                <div className="cart__products">
-                    <CartProduct/>
-                    <CartProduct/>
-                    <CartProduct/>
-                    <CartProduct/>
-                    <CartProduct/>
-                </div>
-                <div className="cart__price">
-                    <strong className="cart__price__text">Subtotal - R$ 12162,40</strong>
-                </div>
+    return(
+        <div className="cart__modal modal__cart__hidden">
+            <div className="cart__top">
+                <button onClick={handleCloseCart} className="return__button">
+                    <i className="fas fa-arrow-left"></i>
+                </button>
+                <strong>Sacola (0)</strong>
             </div>
-        )
-    }
+            <div className="cart__products">
+                <CartProduct/>
+                <CartProduct/>
+                <CartProduct/>
+                <CartProduct/>
+                <CartProduct/>
+            </div>
+            <div className="cart__price">
+                <strong className="cart__price__text">Subtotal - R$ 12162,40</strong>
+            </div>
+        </div>
+    )
 }
 
 export default Cart
