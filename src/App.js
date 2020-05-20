0637c841c6423a2d10a9b77cc3097ff5dc7ae5ca
gabriel-severo <gabriel.ivi@hotmail.com>
2020-05-20 12:48:57 -0300

    Modified component App

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import TopBar from './components/TopBar'
 import Products from './components/Products'
 import Search from './components/Search'
 import Cart from './components/Cart'
 
-class App extends React.Component {
-  constructor(){
-    super()
-    this.state = {products: []}
-  }
+function App() {
+  const [products, setProducts] = useState([])
 
-  async componentDidMount(){
-    const products = await fetch('https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog').then(products => products.json())
-    this.setState({products})
-  }
+  useEffect( () => {
+    async function getProducts(){
+      const products = await fetch('https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog').then(products => products.json())
+      setProducts(products)
+    }
+    getProducts()
+  }, [])
 
-  render(){
     return (
       <React.Fragment>
         <div className="app">
           <TopBar/>
-          <Products products={this.state.products}/>
-          <Search products={this.state.products}/>
+          <Products products={products}/>
+          <Search products={products}/>
           <Cart/>
         </div>
       </React.Fragment>
     )
-  }
 }
 
 export default App;
